edd968f2f1568d3028d085e177b0a764f3504d38
João Pedro Silveira da Costa <joaopedrocosta2004@outlook.com>
2024-04-30 15:30:48 +0100

    .

---
+++ CR/Ficha7_DeepLearningToolbox_CR/trainHeart.m
@@ -0,0 +1,24 @@
+function trainHeart()
+
+clear all;
+close all;
+
+S = readmatrix('heart_train.csv', 'Delimiter', ',', 'DecimalSeparator', '.');
+
+p = S(:,1:end-1)';
+t = S(:,end)';
+
+net = feedforwardnet([10 10 10]); %[10 10 10] rede neuronal com 3 camadas e 10 neuronios cada uma
+
+net.divideFcn = '';
+[net,tr] = train(net,p,t);
+view(net);
+
+y = (net);
+
+y = y>=0.5;
+accuracy = sum(y==t)/length(t);
+disp(['Accuracy = ' num2str(accuracy)]);
+plotperform(tr);
+
+save('nn_heart1.mat', 'net');
