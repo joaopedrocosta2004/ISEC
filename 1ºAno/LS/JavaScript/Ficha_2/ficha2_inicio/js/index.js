65c38901b3de3165404d6bbec24d73a48ebc7aaa
João Costa <148901636+joaopedrocosta2004@users.noreply.github.com>
2025-02-17 17:50:50 +0000

    Alteração do let nas variaveis para const
    Utilizado o prettier por isso das varias atualizacoes no codigo

--- 1ºAno/LS/JavaScript/Ficha_2/ficha2_inicio/js/index.js
+++ 1ºAno/LS/JavaScript/Ficha_2/ficha2_inicio/js/index.js
@@ -1,72 +1,68 @@
 "use strict";
 
-let panelControl = document.getElementById("panel-control");
-let panelGame = document.getElementById("panel-game");
-let btLevel = document.getElementById("btLevel");
-let btPlay = document.getElementById("btPlay");
-let message = document.getElementById("message");
+const panelControl = document.getElementById("panel-control");
+const panelGame = document.getElementById("panel-game");
+const btLevel = document.getElementById("btLevel");
+const btPlay = document.getElementById("btPlay");
+const message = document.getElementById("message");
+// Encontra todas a classes list-item
+const itens = document.querySelectorAll(".list-item");
 
 function reset() {
-    panelGame.style.display = "none";
-    btPlay.disabled = true;
+  panelGame.style.display = "none";
+  btPlay.disabled = true;
 
-    // Habilita o botão Iniciar Jogo caso o level seja diferente de 0
-    if (btLevel.value != "0") {
-        // Habilita o botão Iniciar Jogo 
-        btPlay.disabled = false;
-        // Mostra o painel das cartas do jogo
-        panelGame.style.display = "grid";
-    }
+  // Habilita o botão Iniciar Jogo caso o level seja diferente de 0
+  if (btLevel.value != "0") {
+    // Habilita o botão Iniciar Jogo
+    btPlay.disabled = false;
+    // Mostra o painel das cartas do jogo
+    panelGame.style.display = "grid";
+  }
 
-    // Encontra todas a classes list-item
-    let itens = document.querySelectorAll('.list-item');
-
-    // Quando termina o jogo retira a classe gameStarted
-    itens.forEach(item => {
-        item.classList.remove('gameStarted');
-    });
+  // Quando termina o jogo retira a classe gameStarted
+  itens.forEach((item) => {
+    item.classList.remove("gameStarted");
+  });
 }
 
 function startGame() {
-    // Desabilita o dropdown
-    btLevel.disabled = true;
-    // Muda o texto do botão Iniciar Jogo para Terminar Jogo
-    btPlay.innerHTML = "Terminar Jogo";
+  // Desabilita o dropdown
+  btLevel.disabled = true;
+  // Muda o texto do botão Iniciar Jogo para Terminar Jogo
+  btPlay.innerHTML = "Terminar Jogo";
 
-    // Encontra todas a classes list-item
-    let itens = document.querySelectorAll('.list-item');
+  // Quando começa o jogo adiciona a classe gameStarted
+  itens.forEach((item) => {
+    item.classList.add("gameStarted");
+  });
 
-    // Quando começa o jogo adiciona a classe gameStarted
-    itens.forEach(item => {
-        item.classList.add('gameStarted');
-    });
-
-    // Para que o elemento com o texto “Clique em Iniciar Jogo” não seja visível quando o jogo tiver iniciado
-    message.classList.add('hide');
+  // Para que o elemento com o texto “Clique em Iniciar Jogo” não seja visível quando o jogo tiver iniciado
+  message.classList.add("hide");
 }
 
 function stopGame() {
-    // Habilita o dropdown
-    btLevel.disabled = false;
-    // Muda o texto do botão Iniciar Jogo para Iniciar Jogo
-    btPlay.innerHTML = "Iniciar Jogo";
-    // Para que o elemento com o texto “Clique em Iniciar Jogo” seja visível quando o jogo tiver terminado
-    message.classList.remove('hide');
-    reset();
+  // Habilita o dropdown
+  btLevel.disabled = false;
+  // Muda o texto do botão Iniciar Jogo para Iniciar Jogo
+  btPlay.innerHTML = "Iniciar Jogo";
+  // Para que o elemento com o texto “Clique em Iniciar Jogo” seja visível quando o jogo tiver terminado
+  message.classList.remove("hide");
+  reset();
 }
 
-btLevel.addEventListener("change", function() {
-    reset();
+btLevel.addEventListener("change", function () {
+  reset();
 });
 
 btPlay.addEventListener("click", () => {
-    if (btPlay.innerHTML === "Iniciar Jogo") {
-        startGame();
-    } else {
-        stopGame();
-    }
-})
+  if (btPlay.innerHTML === "Iniciar Jogo") {
+    startGame();
+  } else {
+    stopGame();
+  }
+});
 
-panelGame.addEventListener("click", function() {
-    message.classList.toggle('hide');
+panelGame.addEventListener("click", function () {
+  message.classList.toggle("hide");
 });
