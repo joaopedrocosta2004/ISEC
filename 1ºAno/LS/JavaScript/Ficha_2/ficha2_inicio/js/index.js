380d733c08508ffdb7cd09db6d9512e8381b29be
João Costa <148901636+joaopedrocosta2004@users.noreply.github.com>
2025-02-24 18:17:42 +0000

    Concluida a Ficha 3

--- 1ºAno/LS/JavaScript/Ficha_2/ficha2_inicio/js/index.js
+++ 1ºAno/LS/JavaScript/Ficha_2/ficha2_inicio/js/index.js
@@ -7,6 +7,29 @@ const btPlay = document.getElementById("btPlay");
 const message = document.getElementById("message");
 // Encontra todas a classes list-item
 const elementos = document.querySelectorAll(".list-item");
+const cards = panelGame.querySelectorAll(".card");
+
+const cardsLogos = [
+  "angular",
+  "bootstrap",
+  "html",
+  "javascript",
+  "vue",
+  "svelte",
+  "react",
+  "css",
+  "backbone",
+  "ember",
+];
+
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+};
 
 function reset() {
   panelGame.style.display = "none";
@@ -39,6 +62,28 @@ function startGame() {
 
   // Para que o elemento com o texto “Clique em Iniciar Jogo” não seja visível quando o jogo tiver iniciado
   message.classList.add("hide");
+
+  // console.table(cardsLogos);
+  shuffleArray(cardsLogos);
+  // console.table(cardsLogos);
+
+  // Obter os primeiros 3 elementos do array inicial
+  const newCardLogos = cardsLogos.slice(0, 3);
+  // Duplicar o array para criar os pares
+  newCardLogos.push(...newCardLogos);
+  // Embaralha o array
+  shuffleArray(newCardLogos);
+
+  cards.forEach((card, index) => {
+    // Altera o data-logo da carta
+    card.dataset.logo = newCardLogos[index];
+
+    // Altera a imagem da carta
+    const img = card.querySelector(".card-front");
+    img.src = `images/${newCardLogos[index]}.png`;
+    // Adiciona o evento a todas as cartas do jogo
+    card.addEventListener("click", flipCard);
+  });
 }
 
 function stopGame() {
@@ -48,7 +93,30 @@ function stopGame() {
   btPlay.innerHTML = "Iniciar Jogo";
   // Para que o elemento com o texto “Clique em Iniciar Jogo” seja visível quando o jogo tiver terminado
   message.classList.remove("hide");
+
+  // Remove o evento de todas as cartas quando o jogo termina
+  cards.forEach((card) => {
+    card.removeEventListener("click", flipCard);
+  });
+
+  hideCards();
   reset();
+}
+
+function showCards() {
+  for (const card of cards) {
+    card.classList.add("flipped");
+  }
+}
+
+function hideCards() {
+  for (const card of cards) {
+    card.classList.remove("flipped");
+  }
+}
+
+function flipCard() {
+  this.classList.toggle("flipped");
 }
 
 btLevel.addEventListener("change", function () {
@@ -66,3 +134,5 @@ btPlay.addEventListener("click", () => {
 panelGame.addEventListener("click", function () {
   message.classList.toggle("hide");
 });
+
+reset();
