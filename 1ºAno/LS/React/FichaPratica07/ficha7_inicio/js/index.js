0abe51a67cbd8aa1fa1b291d6cc24ebdf738e7f8
João Costa <148901636+joaopedrocosta2004@users.noreply.github.com>
2025-03-24 18:09:38 +0000

    Alterado a resoluçao

--- 1ºAno/LS/React/FichaPratica07/ficha7_inicio/js/index.js
+++ 1ºAno/LS/React/FichaPratica07/ficha7_inicio/js/index.js
@@ -16,20 +16,41 @@
 //   )
 // }
 
+function App() {
+  return (
+    <div className="container">
+      <InfoComponent
+        img={"images/ember.png"}
+        link={"https://emberjs.com/"}
+        title={"Ember"}
+        description={
+          "Ember.js  is  a  productive,  battle-tested  JavaScript  framework  for  building  modern  web applications. It includes everything you need to build rich UIs that work on any device."
+        }
+      />
+      <InfoComponent
+        img={"images/vue.png"}
+        link={"https://vuejs.org/"}
+        title={"Vue"}
+        description={
+          " Vue is a JavaScript  framework for building user  interfaces. It  is built on top of standard HTML, CSS and JavaScript, and provides a declarative and component-based programming model that helps you efficiently develop user interfaces, be it simple or complex."
+        }
+      />
+    </div>
+  );
+}
+
 function InfoComponent(props) {
   return (
-    <div class="container">
-      <div class="wrapper">
-        <div class="logo">
-          <img src={props.img} alt="React" />
-        </div>
-        <div class="text">
-          <h2>{props.title}</h2>
-          <p>{props.description}</p>
-          <a href={props.link} target="_blank">
-            Ler mais
-          </a>
-        </div>
+    <div className="wrapper">
+      <div className="logo">
+        <img src={props.img} alt={props.title} />
+      </div>
+      <div className="text">
+        <h2>{props.title}</h2>
+        <p>{props.description}</p>
+        <a href={props.link} target="_blank">
+          Ler mais
+        </a>
       </div>
     </div>
   );
@@ -42,22 +63,7 @@ ReactDOM.render(
     <LinguagensScript name="Antonio Antunes"/>
     <LinguagensScript name="Joao Antunes"/>
     <LinguagensScript name="Ze Antunes"/> */}
-    <InfoComponent
-      img={"images/ember.png"}
-      link={"https://emberjs.com/"}
-      title={"Ember"}
-      description={
-        "Ember.js  is  a  productive,  battle-tested  JavaScript  framework  for  building  modern  web applications. It includes everything you need to build rich UIs that work on any device."
-      }
-    />
-    <InfoComponent
-      img={"images/vue.png"}
-      link={"https://vuejs.org/"}
-      title={"Vue"}
-      description={
-        " Vue is a JavaScript  framework for building user  interfaces. It  is built on top of standard HTML, CSS and JavaScript, and provides a declarative and component-based programming model that helps you efficiently develop user interfaces, be it simple or complex."
-      }
-    />
+    <App />
   </React.StrictMode>,
   containerRoot
 );
