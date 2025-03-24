b3821ccf4a639f1fc40a879085d6be8bb7ecda3e
João Costa <148901636+joaopedrocosta2004@users.noreply.github.com>
2025-03-24 16:53:38 +0000

    Adicionados ficheiros da aula 7

---
+++ 1ºAno/LS/React/FichaPratica07/ficha7_inicio/js/index.js
@@ -0,0 +1,11 @@
+function LinguagensScript() {
+  return <h1>Linguagens Script!</h1>;
+}
+
+const containerRoot = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <LinguagensScript />
+  </React.StrictMode>,
+  containerRoot
+);
